c5348f082b141b03d9639ca5ae62d69a0e223bb3
wnasim <waseem.nasim@gmail.com>
2016-01-26 19:04:11 -0500

    Waseem's Changes
    Assignment 2
    
    Created 3 Winer Jackets and added 3 corresponding Buttons. Clicking on
    a button displays the colour of the corresponding Jacket.

--- app/src/main/java/ca/khuddam/android_winter2016/MainActivity.java
+++ app/src/main/java/ca/khuddam/android_winter2016/MainActivity.java
@@ -4,11 +4,14 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.view.View;
+import android.widget.LinearLayout;
 
 
 public class MainActivity extends AppCompatActivity {
 
     TextView topLine;
+    WinterJacket smallJacket, mediumJacket, largeJacket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,22 +19,71 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Wire a connection to the top TextView
-        topLine = (TextView)findViewById(R.id.top_line);
-
-        topLine.setText(topLine.getText() + " - Some dynamic stuff I control");
+        //topLine = (TextView)findViewById(R.id.top_line);
+        //topLine.setText(topLine.getText() + " - Some dynamic stuff I control");
 
         // Creating an actual instance of a Jacket from the blueprint
         // that we defined in WinterJacket.java
-        WinterJacket smallJacket = new WinterJacket();
+        smallJacket = new WinterJacket();
         smallJacket.setColor(WinterJacket.JacketColor.BLUE);
 
-        switch(smallJacket.getColor()) {
-            // Set the topline text color based on jackcolor
-            // using html type hex color values
-            case BLACK: topLine.setTextColor(Color.BLACK); break;
-            case BLUE: topLine.setTextColor(Color.BLUE); break;
-            case RED: topLine.setTextColor(Color.RED); break;
+        // Updates by Waseem N. for assignment 2
+
+        // Create a medium sized jacket in RED color.
+        mediumJacket = new WinterJacket();
+        mediumJacket.setColor(WinterJacket.JacketColor.RED);
+
+        // Create a large sized jacket in BLACK color.
+        largeJacket = new WinterJacket();
+        mediumJacket.setColor(WinterJacket.JacketColor.BLACK);
+    }
+
+    public void onSmallButtonClick(View view){
+        //Change the background to the colour of the small jacket
+        LinearLayout LL;
+        LL = (LinearLayout) findViewById(R.id.Layout);
+        LL.setBackgroundColor( getColorValue(smallJacket.getColor()));
+    }
+
+    public void onMediumButtonClick(View view){
+        //Change the background to the colour of the medium jacket
+        LinearLayout LL;
+        LL = (LinearLayout) findViewById(R.id.Layout);
+        LL.setBackgroundColor( getColorValue(mediumJacket.getColor()));
+    }
+
+    public void onLargeButtonClick(View view){
+        //Change the background to the colour of the medium jacket
+        LinearLayout LL;
+        LL = (LinearLayout) findViewById(R.id.Layout);
+        LL.setBackgroundColor( getColorValue(largeJacket.getColor()));
+    }
+
+
+    /**
+     *
+     * @param jc The colour of the Jacket.
+     * @return Returns the corresponding int color value of the Jacket.
+     */
+    private int getColorValue(WinterJacket.JacketColor jc)
+    {
+        int col;
+        switch(jc) {
+
+            case BLACK:
+                col =  Color.BLACK;
+                break;
+            case RED:
+                col = Color.RED;
+                break;
+            case BLUE:
+                col = Color.BLUE;
+                break;
+            default:
+                col= Color.BLACK;
+                break;
         }
+        return col;
 
     }
 }
