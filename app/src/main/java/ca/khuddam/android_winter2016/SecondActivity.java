3f4e412542fa8dca4655dc33eb46e1f151c56880
ehsanelahimalik <ehsanelahimalik@gmail.com>
2016-02-02 16:20:10 -0500

    Q4. Completed
    Assignment completed.
    
    For purpose of check the work done in Q1~Q3; I have set an
    OnTouchListener on root Layout which will open the Main Activity (1st
    Activity).
    
    All questions are submitted as separate commits so they can be checked
    in their respective state as well.

--- app/src/main/java/ca/khuddam/android_winter2016/SecondActivity.java
+++ app/src/main/java/ca/khuddam/android_winter2016/SecondActivity.java
@@ -1,13 +1,27 @@
 package ca.khuddam.android_winter2016;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class SecondActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        RelativeLayout root = (RelativeLayout)findViewById(R.id.second_activity_root);
+
+        root.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Intent intentOpenFirstActicity = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intentOpenFirstActicity);
+                return false;
+            }
+        });
     }
 }
