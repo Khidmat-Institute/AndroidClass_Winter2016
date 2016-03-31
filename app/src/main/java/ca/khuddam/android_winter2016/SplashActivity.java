e11e116a0e2f91323efbe74b1823d5f7f8d67738
roomachev <roomachev@gmail.com>
2016-03-30 20:47:28 -0400

    Added Splash screen to app

---
+++ app/src/main/java/ca/khuddam/android_winter2016/SplashActivity.java
@@ -0,0 +1,30 @@
+package ca.khuddam.android_winter2016;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    // Every App Process has a Main UI Thread
+    // Mainn thread is monitoring a message queue
+
+    static final int SPLASH_DELAY = 5*1000;
+    Handler mHandler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_DELAY);
+    }
+}
